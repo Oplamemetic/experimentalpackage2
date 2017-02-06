d16633c73d762d67b3af6cb240e1c6af472088bd
Oplamemetic <oplamemetic@gmail.com>
2017-02-06 16:36:57 -0200

    update

---
+++ cm-fm-exp01/mom/WIL/teste-wil.R
@@ -0,0 +1,14 @@
+dados <- read.table("/home/note/000-analise-exp-parte-2/cm-fm/mom/WIL/IGD.csv", head=T, sep=",", dec=".")
+
+Bestof2<-dados$B2
+Genetico<-dados$GN
+
+shapiro.test(Bestof2)
+shapiro.test(Genetico)
+
+result<-wilcox.test(Bestof2,Genetico)
+
+result<-t.test(Bestof2,Genetico)
+result
+
+boxplot(Bestof2, Genetico, names=c("Bestof2", "Genetico"))
---
+++ coe-aclass-exp02/mom/WIL/teste-wil.R
@@ -0,0 +1,14 @@
+dados <- read.table("/home/note/000-analise-exp-parte-2/coe-aclass/mom/WIL/IGD.csv", head=T, sep=",", dec=".")
+
+Bestof2<-dados$B2
+Genetico<-dados$GN
+
+shapiro.test(Bestof2)
+shapiro.test(Genetico)
+
+result<-wilcox.test(Bestof2,Genetico)
+
+result<-t.test(Bestof2,Genetico)
+result
+
+boxplot(Bestof2, Genetico, names=c("Bestof2", "Genetico"))
